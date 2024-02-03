a9a29ec672903b49742366a57836b6a4ab19fc57
Orlan <orlan.forshaw22@ic.ac.uk>
2024-02-03 17:07:51 +0000

    first python script to run tesseract

---
+++ Tesseract.py
@@ -0,0 +1,13 @@
+import pytesseract
+from PIL import Image
+
+# Open the image
+image = Image.open('image.png')
+
+# Perform OCR
+custom_config = r'--oem 3 --psm 6 -l eng'
+text = pytesseract.image_to_string(image, config=custom_config)
+
+print(text)
+
+
